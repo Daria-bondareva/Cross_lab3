84e7d39dd01e597f612440e55940c2b8d523f163
Daria-Bondareva <dasha.bondareva05@gmail.com>
2024-05-31 16:42:18 +0300

    init lab 3 cross plat

---
+++ src/test/java/com/bondareva/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bondareva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("spring-boot-test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
